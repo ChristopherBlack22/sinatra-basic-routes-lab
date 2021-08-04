31027c7942596528f85cb6c11f7a87273b8c08c1
Christopher <christopher.black.2209@googlemail.com>
2021-08-05 00:50:21 +0100

    commit tree 2aa36b64f5606b2fe3f1e97bd8c778edbdb96c9f

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get "/name" do
+        "My name is Chirstopher Black."
+    end 
+
+    get "/hometown" do
+        "My hometown is currently London."
+    end 
+
+    get "/favorite-song" do
+        "My favorite song is Chronic Funkbots."
+    end 
+
 end
